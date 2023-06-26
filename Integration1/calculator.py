d4370acb1b46a9b839716ea4b9d9e78629a2655e
Charlet3962 <135926680+Charlet3962@users.noreply.github.com>
2023-06-26 01:26:06 -0400

    Add files via upload

---
+++ Integration1/calculator.py
@@ -0,0 +1,13 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        raise ValueError("Cannot divide by zero")
+    return num1 / num2
